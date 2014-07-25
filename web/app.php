e1a7fe0f86710d9fe0b1c211dce04403f4c8a828
dinhanhtuan <dinhanhtuan24@gmail.com>
2014-07-26 01:12:48 +0700

    Fresh finished Episode 1

--- web/app.php
+++ web/app.php
@@ -4,14 +4,13 @@ use Symfony\Component\ClassLoader\ApcClassLoader;
 use Symfony\Component\HttpFoundation\Request;
 
 $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
+umask(0000);
 
-// Use APC for autoloading to improve performance.
-// Change 'sf2' to a unique prefix in order to prevent cache key conflicts
-// with other applications also using APC.
+// Use APC for autoloading to improve performance
+// Change 'sf2' by the prefix you want in order to prevent key conflict with another application
 /*
-$apcLoader = new ApcClassLoader('sf2', $loader);
-$loader->unregister();
-$apcLoader->register(true);
+$loader = new ApcClassLoader('sf2', $loader);
+$loader->register(true);
 */
 
 require_once __DIR__.'/../app/AppKernel.php';
@@ -20,9 +19,6 @@ require_once __DIR__.'/../app/AppKernel.php';
 $kernel = new AppKernel('prod', false);
 $kernel->loadClassCache();
 //$kernel = new AppCache($kernel);
-
-// When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
-//Request::enableHttpMethodParameterOverride();
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
